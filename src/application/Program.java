2e38e9cc837becb413a6c639aeb0394b390d288d
Wellington Braga <wellinbraga@gmail.com>
2022-06-19 20:15:43 -0300

    More one

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,43 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import Chess.ChessException;
 import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		// no ChessMatch define o Boar 8 por 8 
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.perfomChessMove(source, target);
+				System.out.println("["+ capturedPiece +"]");
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+		}
 		
 	}
 
